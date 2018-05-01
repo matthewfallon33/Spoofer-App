18f8553dba567de9cc7ed6a4087a5e02fca4ffc7
Matthew <matthewfallon33@gmail.com>
2018-05-01 15:50:41 +0100

    Trying to save users to db

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var app = express();
 
 var User = require("./model/user");
 
-User.find({name:"Joe Bloggs"}, function(err, user){
-  if(err){
-    console.log(err);
-  } else{
-    console.log(user[0]);
-  }
-})
+// User.find({name:"Joe Bloggs"}, function(err, user){
+//   if(err){
+//     console.log(err);
+//   } else{
+//     console.log(user[0]);
+//   }
+// })
 
 mongoose.connect(process.env.DBPATH)
   .then(function(data){
@@ -25,7 +25,8 @@ mongoose.connect(process.env.DBPATH)
     console.log(err);
   }
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json()); // to support JSON bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -43,18 +44,31 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
 
-  console.log(User);
-  User.firstname = req.body.firstname;
-  User.surname = req.body.surname;
-  User.questions.q1 = req.body.q1;
-  User.questions.q2 = req.body.q2;
-  User.questions.q3 = req.body.q3;
-  User.questions.q4 = req.body.q4;
-  User.questions.q5 = req.body.q5;
+  // console.log(User);
+  console.log(req.body);
+  strToBoolean(req.body.q1Spoof);
+  strToBoolean(req.body.q2Spoof)
+  strToBoolean(req.body.q3Spoof);
+  console.log(req.body.q3Spoof);
 
-  User.save(err => console.log(err));
+  console.log("User:\n" + User());
+  // User.firstname = req.body.firstname;
+  // User.surname = req.body.surname;
+  // User.questions.q1.statement = req.body.q1;
+  // User.questions.q1.spoof = strToBoolean(req.body.q1Spoof);
+  // User.questions.q2.statement = req.body.q2;
+  // User.questions.q2.spoof = strToBoolean(req.body.q2Spoof);
+  // User.questions.q3.statement = req.body.q3;
+  // User.questions.q3.spoof = strToBoolean(req.body.q3Spoof);
+  //   User.save(err => console.log("Did not save to database: \n" + err));
 
 });
+
+function strToBoolean(string){
+   switch(string.toLowerCase().trim()){
+    case "false": case "no": case "0": case "": return false; default: return true;}
+}
+// ^ put in utils folder/file
 
 app.listen(process.env.PORT, () => {
   console.log("App Listening on Port: " + process.env.PORT);
